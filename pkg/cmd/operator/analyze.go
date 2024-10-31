32332f09e3d774272a24d56d2e7fea97dcb7b8ce
Tytus Walężak <tw448537@students.mimuw.edu.pl>
2024-10-31 22:50:08 +0100

    Resolve (most)  add analyze scaffold PR comments

--- pkg/cmd/operator/analyze.go
+++ pkg/cmd/operator/analyze.go
@@ -1,46 +1,19 @@
 package operator
 
-import(
+import (
 	"fmt"
+	"os"
 
-	"github.com/spf13/cobra"
 	"github.com/scylladb/scylla-operator/pkg/genericclioptions"
+	"github.com/spf13/cobra"
 	"k8s.io/apimachinery/pkg/util/errors"
+	cliflag "k8s.io/component-base/cli/flag"
+	"k8s.io/klog/v2"
 )
 
-type AnalyzeOptions struct{
-	Mode string
-	GatherDirPath string
-}
-
-func (o *AnalyzeOptions) Validate() error {
-	var errs []error
-
-	if len(o.Mode) != 0 && o.Mode != "live" && o.Mode != "gathered"{
-		errs = append(errs, fmt.Errorf("Invalid mode. Use either 'live' or 'gathered'" ))
-	}
-
-	if o.Mode == "gathered" && len(o.GatherDirPath) == 0 {
-		errs = append(errs, fmt.Errorf("dir-path not specified"))
-	}
-
-	if len(o.Mode) == 0 && len(o.GatherDirPath) == 0 {
-		errs = append(errs, fmt.Errorf("Set flag use=live or specify directory path for gathered data"))
-	}
-
-	return errors.NewAggregate(errs)
-}
-
-func (o *AnalyzeOptions) Complete() error {
-	return nil
-}
-
-func (o *AnalyzeOptions) Run(streams genericclioptions.IOStreams, cmd *cobra.Command) error {
-	fmt.Printf("Analyze run with options: mode='%s', gatherDirPath='%s'\n", o.Mode, o.GatherDirPath)
-
-	// TODO
-
-	return nil
+type AnalyzeOptions struct {
+	Kubeconfig  string
+	ArchivePath string
 }
 
 func NewAnalyzeOptions(streams genericclioptions.IOStreams) *AnalyzeOptions {
@@ -51,25 +24,22 @@ func NewAnalyzeCmd(streams genericclioptions.IOStreams) *cobra.Command {
 	o := NewAnalyzeOptions(streams)
 
 	cmd := &cobra.Command{
-		Use: "analyze",
-		Short: "Run the scylla analyze.",
-		Long: "Run the scylla anallyze to analyze either live cluster or gathered data",
-		RunE: func(cmd *cobra.Command, args []string) error{
+		Use:   "analyze",
+		Short: "Run scylla-operator analyze.",
+		Long:  "Run the scylla anallyze to analyze either live cluster or gathered data",
+		RunE: func(cmd *cobra.Command, args []string) error {
 
 			err := o.Validate()
-
 			if err != nil {
 				return err
 			}
 
 			err = o.Complete()
-
 			if err != nil {
 				return err
 			}
 
 			err = o.Run(streams, cmd)
-
 			if err != nil {
 				return err
 			}
@@ -78,8 +48,38 @@ func NewAnalyzeCmd(streams genericclioptions.IOStreams) *cobra.Command {
 		},
 	}
 
-	cmd.Flags().StringVarP(&o.Mode, "mode", "", o.Mode, "Specify the mode: live or gathered")
-	cmd.Flags().StringVarP(&o.GatherDirPath, "dirPath", "", o.GatherDirPath, "Specify the directory path for gathered data")
+	cmd.Flags().StringVarP(&o.Kubeconfig, "kubeconfig", "", o.Kubeconfig, "Kubeconfig")
+	cmd.Flags().StringVarP(&o.ArchivePath, "archive-path", "", o.ArchivePath, "Specify the directory path for archive created by must-gather")
 
 	return cmd
+}
+
+func (o *AnalyzeOptions) Validate() error {
+	var errs []error
+
+	_, err := os.Stat(o.ArchivePath)
+	if err != nil {
+		errs = append(errs, fmt.Errorf("archive-path is not a valid filepath"))
+	}
+
+	if len(o.Kubeconfig) != 0 && len(o.ArchivePath) != 0 {
+		errs = append(errs, fmt.Errorf("kubeconfig and archive-path can't both be set"))
+	}
+
+	if len(o.Kubeconfig) == 0 && len(o.ArchivePath) == 0 {
+		errs = append(errs, fmt.Errorf("kubeconfig and archive-path can't bot be empty"))
+	}
+
+	return errors.NewAggregate(errs)
+}
+
+func (o *AnalyzeOptions) Complete() error {
+	return nil
+}
+
+func (o *AnalyzeOptions) Run(streams genericclioptions.IOStreams, cmd *cobra.Command) error {
+	klog.Infof("%s run", cmd.Name())
+	cliflag.PrintFlags(cmd.Flags())
+
+	return nil
 }
