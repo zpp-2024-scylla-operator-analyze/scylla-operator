ad0273c481eddf35c5e46b1f7090c2b31ef176ea
Tytus Walężak <tw448537@students.mimuw.edu.pl>
2024-11-07 20:22:07 +0100

    Add mustgather support to analyze command
    Add unit test for creating indexers from archive
    
    Add NewDataSourceFromFS function which creates a dataSource from a map
    of indexers
    
    Remove some test parameters, change test logic slightly
    
    Change lister creation to use empty indexers instrad of nils
    
    Delete unnecessary new lines and resolve other minor comments
    
    Remove isYamlFile function, add option to set a specified decoder, pass testing.t to checkIndexer
    
    Change file content and expected indexer objects to inline
    
    Delete multiple tests testing the same thing
    
    Add disable-strict-encoding flag

---
+++ pkg/analyze/archivereader.go
@@ -0,0 +1,80 @@
+package analyze
+
+import (
+	"context"
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"reflect"
+
+	scyllav1 "github.com/scylladb/scylla-operator/pkg/api/scylla/v1"
+	scyllav1listers "github.com/scylladb/scylla-operator/pkg/client/scylla/listers/scylla/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	corev1listers "k8s.io/client-go/listers/core/v1"
+	"k8s.io/client-go/tools/cache"
+)
+
+func getIndexerForType(indexers map[reflect.Type]cache.Indexer, objType reflect.Type) cache.Indexer {
+	if indexer, exists := indexers[objType]; exists {
+		return indexer
+	}
+	// Create new indexer for this type if it doesn't exist
+	indexer := cache.NewIndexer(cache.MetaNamespaceKeyFunc, cache.Indexers{})
+	indexers[objType] = indexer
+	return indexer
+}
+
+func IndexersFromFS(fsys fs.FS, decoder runtime.Decoder) (map[reflect.Type]cache.Indexer, error) {
+	indexers := make(map[reflect.Type]cache.Indexer)
+
+	err := fs.WalkDir(fsys, ".", func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if d.IsDir() || filepath.Ext(path) != ".yaml" {
+			return nil
+		}
+		content, err := fs.ReadFile(fsys, path)
+		if err != nil {
+			return fmt.Errorf("can't read file %q: %w", path, err)
+		}
+		obj, _, err := decoder.Decode(content, nil, nil)
+
+		if err != nil {
+			if !runtime.IsNotRegisteredError(err) {
+				return fmt.Errorf("can't deserialize file %q: %w", path, err)
+			}
+			return nil
+		}
+		objType := reflect.TypeOf(obj)
+		indexer := getIndexerForType(indexers, objType)
+		err = indexer.Add(obj)
+		if err != nil {
+			return fmt.Errorf("can't add object to indexer: %w", err)
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("can't walk the file tree: %w", err)
+	}
+	return indexers, nil
+}
+
+func NewDataSourceFromFS(ctx context.Context, archivePath string, decoder runtime.Decoder) (*DataSource, error) {
+	indexers, err := IndexersFromFS(os.DirFS(archivePath), decoder)
+	if err != nil {
+		return nil, fmt.Errorf("can't build indexers from fs: %w", err)
+	}
+
+	return &DataSource{
+		PodLister:            corev1listers.NewPodLister(getIndexerForType(indexers, reflect.TypeOf(&corev1.Pod{}))),
+		ServiceLister:        corev1listers.NewServiceLister(getIndexerForType(indexers, reflect.TypeOf(&corev1.Service{}))),
+		SecretLister:         corev1listers.NewSecretLister(getIndexerForType(indexers, reflect.TypeOf(&corev1.Secret{}))),
+		ConfigMapLister:      corev1listers.NewConfigMapLister(getIndexerForType(indexers, reflect.TypeOf(&corev1.ConfigMap{}))),
+		ServiceAccountLister: corev1listers.NewServiceAccountLister(getIndexerForType(indexers, reflect.TypeOf(&corev1.ServiceAccount{}))),
+		ScyllaClusterLister:  scyllav1listers.NewScyllaClusterLister(getIndexerForType(indexers, reflect.TypeOf(&scyllav1.ScyllaCluster{}))),
+	}, nil
+}
